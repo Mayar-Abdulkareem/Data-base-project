b1e651a57758abbd3409ff06706a9bee6f50a47d
Mayar-Abdulkareem <mayarimad70@gmail.com>
2022-01-03 20:13:07 +0300

    add files

---
+++ src/sample/EAccessoriesController.java
@@ -0,0 +1,39 @@
+package sample;
+
+import com.jfoenix.controls.JFXButton;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class EAccessoriesController {
+    @FXML
+    private AnchorPane pane;
+    @FXML
+    public void exitApplication(ActionEvent event) {
+        System.exit(0);
+    }
+    @FXML
+    public void minimizeApplication(ActionEvent event)  {
+        Stage stage = (Stage) pane.getScene().getWindow();
+        stage.setIconified(true);
+    }
+    @FXML
+    public void switchTheScene(ActionEvent event) throws IOException {
+        System.out.println(LogOutController.flag+((JFXButton)event.getSource()).getText().replaceAll("\\s", "")+".fxml");
+        Parent root = FXMLLoader.load(getClass().getResource(LogOutController.flag+((JFXButton)event.getSource()).getText().replaceAll("\\s", "")+".fxml"));
+        Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root);
+        scene.setFill(Color.TRANSPARENT);
+        stage.setScene(scene);
+        stage.show();
+        stage.setResizable(false);
+    }
+}
---
+++ src/sample/ordersTable.java
@@ -0,0 +1,26 @@
+package sample;
+
+
+public class ordersTable {
+    public Integer OID,totalPrice;
+    public String date ;
+
+    public ordersTable(int oid ,int totalPrice , String date ) {
+        this.OID = oid;
+        this.totalPrice = totalPrice ;
+        this.date = new String(date)  ;
+    }
+
+    public Integer getOID () {
+        return OID ;
+    }
+
+    public Integer getTotalPrice() {
+        return totalPrice ;
+    }
+
+    public String getDate () {
+        return date ;
+    }
+
+}
